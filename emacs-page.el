709cf1f86fd24c9137e03166cc5399f67cb3ca7a
Bart Massey <bart@cs.pdx.edu>
2008-04-20 17:25:36 -0700

    changed to be page mode; many code fixups and extensions

--- emacs-page.el
+++ emacs-page.el
@@ -11,29 +11,69 @@
 ;; are user-defined subsets of a buffer.
 ;; Bart Massey 2008/4/17
 
-;; make the keymap
-(defun page-build-mode-map ()
-  "Build the mode map for page mode."
+;; The page separator string.
+(defvar page-mode-separator "\C-l"
+  "The page separator string.")
+
+(defun page-mode-make-separator-pattern (SEP)
+  "Make SEP into a line pattern with optional trailing whitespace."
+  (concat "^" SEP "[ \C-i]*$"))
+
+;; The page separator pattern.
+(defvar page-mode-separator-pattern
+  (page-mode-make-seperator-pattern page-mode-separator)
+  "Regexp describing the page separator line.
+This expression currently must start with ^ and end with $,
+thus marking an entire line.")
+
+(defun page-mode-set-separator (SEP)
+  "Set page-mode-separator to SEP.
+Updates page-mode-separator-pattern."
+  (interactive "s" "Separator: ")
+  (setq page-mode-separator SEP)
+  (setq page-mode-separator-pattern
+	(page-mode-make-seperator-pattern SEP)))
+
+(defun page-mode-build-keymap ()
+  "Build the mode keymap for page mode."
   (let  ((map (make-keymap)))
-    (define-key map "\C-c\C-n" 'next-page)
-    (define-key map "\C-c\C-p" 'previous-page)
+    (define-key map "\C-c\C-n" 'page-mode-next-page)
+    (define-key map "\C-c\C-p" 'page-mode-previous-page)
+    (define-key map "\C-cs" 'page-mode-set-separator)
     map))
 
-;; Actually display the current page.
+(define page-mode-search-backward ()
+  "Back up to previous page separator."
+  (re-search-backward page-mode-separator-pattern nil t))
+
+(define page-mode-search-forward ()
+  "Move forward to end of current page." 
+  (re-search-forward page-mode-separator-pattern nil t))
+
+;; Actually show the current page.
 ;; Must be widened at entry,
 ;; will be narrowed at exit.
-(defun display-page ()
+(defun page-mode-show-current ()
   (let (start (here (point)))
-    (if (re-search-backward "^\C-l$" nil t)
+    (if (page-mode-search-backward)
 	(beginning-of-line 2)
         (goto-char (point-min)))
     (setq start (point))
-    (if (re-search-forward "^\C-l$" nil t)
+    (if (page-mode-search-forward)
 	(beginning-of-line nil)
         (goto-char (point-max)))
     (narrow-to-region start (point))
     (goto-char (point-min))
     (set-window-start nil (point))))
+
+(define page-mode-setup ()
+  "Setup for entering or leaving page mode."
+  (if page-mode
+      (progn
+	(delete-other-windows)
+	(widen)
+	(page-mode-show-current))
+      (widen)))
 
 ;; Define the mode.
 ;; When mode is activated,
@@ -42,29 +82,25 @@
 ;; widen.
 (define-minor-mode page-mode
   "Toggle minor mode for display of pages."
-  nil " Page" (page-build-mode-map)
-  (widen)
-  (if page-mode
-      (progn
-	(delete-other-windows)
-	(display-page))))
+  nil " Page" (page-mode-build-keymap)
+  (make-variable-buffer-local page-mode-separator)
+  (make-variable-buffer-local page-mode-separator-pattern)
+  (page-mode-setup))
 
 ;; move to the next page
-(defun next-page ()
+(defun page-mode-next-page ()
   "Go to next page."
   (interactive)
   (widen)
-  (if (re-search-forward "^\C-l$" nil t)
-      (beginning-of-line 2)
-      (point-max))
-  (display-page))
+  (if (page-mode-search-forward)
+      (beginning-of-line 2))
+  (page-mode-show-current))
 
 ;; move to the previous page
-(defun previous-page ()
+(defun page-mode-previous-page ()
   "Go to previous page."
   (interactive)
   (widen)
-  (if (re-search-backward "^\C-l$" nil t)
-      (beginning-of-line nil)
-      (point-min))
-  (display-page))
+  (if (page-mode-search-backward)
+      (beginning-of-line nil))
+  (page-mode-show-current))
